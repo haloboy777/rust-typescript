25b3b48c2cef06e759b9196d1b9b2fecf4f2dd37
Ayush Singh <ayush@intervue.io>
2023-06-16 00:11:04 +0530

    AS - touching lifetimes

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ fn add_one(item: &mut Item) {
     item.count += 1
 }
 
-fn main() {
-    let mut item = Item { count: 1 };
+fn print_all(items: &Vec<Item>) {
+    for item in items {
+        println!("{:?}", item)
+    }
+}
 
-    println!("{:?}", item);
-    add_one(&mut item);
-    println!("{:?}", item);
+fn main() {
+    let mut items = vec![Item { count: 1 }, Item { count: 2 }];
+    let one = items.get_mut(0);
+    let two = items.get_mut(1);
+    println!("{:?}", two);
 }
