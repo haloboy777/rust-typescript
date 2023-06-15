e3e14d82cfb8349826e6bf76e969a437fc4d4409
Ayush Singh <ayush@intervue.io>
2023-06-15 23:53:46 +0530

    AS - muttable reference

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[derive(Debug)]
+struct Item {
+    count: isize,
+}
+
+fn add_one(item: &mut Item) {
+    item.count += 1
+}
+
 fn main() {
-    
+    let mut item = Item { count: 1 };
+
+    println!("{:?}", item);
+    add_one(&mut item);
+    println!("{:?}", item);
 }
