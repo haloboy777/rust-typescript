635ed3f707097bc708bfda2f3865003ce24b329d
Ayush Singh <ayush@intervue.io>
2023-06-16 00:45:00 +0530

    AS - behaviour of map in rust

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-#[derive(Debug)]
-struct Item {
-    count: isize,
-}
-
-fn add_one(item: &mut Item) {
-    item.count += 1
-}
-
-fn print_all(items: &Vec<Item>) {
-    for item in items {
-        println!("{:?}", item)
-    }
-}
-
 fn main() {
-    let mut items = vec![Item { count: 1 }, Item { count: 2 }];
-    let one = items.get_mut(0);
-    let two = items.get_mut(1);
-    println!("{:?}", two);
+    let items = vec![1, 2, 3].iter().map(|x| x+1);
+    println!("{:?}", items);
+
+    /*
+    This code doesn't work
+
+    lets go line by line here
+
+    - A place to hold our vector which will eventually come
+    let items = 
+    
+    - A temporary place in memory with [1,2,3] values arranged in a vector
+    vec![1,2,3]
+
+    - creating an iterator with reference to items in vector
+    - let items: Iter<'_, i32>
+    .iter()
+
+    - Now for the crazy part..
+    - Map: Map is basically a data structure which holds
+        = the function to apply and
+        = the data to apply it on
+
+    - and once we start iterating over it the iterator
+    - then it will start to execute the mapped function for us.
+    */
 }
