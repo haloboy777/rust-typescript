8b28c7cf3b87cb28fc08708ae213606fe0b20fe3
Ayush Singh <ayush@intervue.io>
2023-06-16 01:07:20 +0530

    AS - life changed moment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let items = vec![1, 2, 3].iter().map(|x| x+1);
-    println!("{:?}", items);
+    // let items = vec![1, 2, 3].iter().map(|x| x+1);
+    // println!("{:?}", items);
 
     /*
     This code doesn't work
@@ -8,8 +8,8 @@ fn main() {
     lets go line by line here
 
     - A place to hold our vector which will eventually come
-    let items = 
-    
+    let items =
+
     - A temporary place in memory with [1,2,3] values arranged in a vector
     vec![1,2,3]
 
@@ -25,4 +25,18 @@ fn main() {
     - and once we start iterating over it the iterator
     - then it will start to execute the mapped function for us.
     */
+
+    // Now the working data
+
+    let data = vec![1, 2, 3];
+    let item_mapped_iter = data.iter().map(|x| x * 2);
+
+    // let item_mapped_iter: Map<Iter<'_, i32>, impl Fn(&i32) -> i32>
+
+    // MAP IS JUST A MAP!!!!
+    // ARE YOU FUCKING KIDDING ME!?!?!?!?!?!?
+
+    for item in item_mapped_iter {
+        println!("{item}")
+    }
 }
